66f70e8c1c1336c62987722bdf59cd1bc6188941
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-10-23 13:33:34 -0400

    Completed UI for LoginVC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Adil Jiwani on 2017-10-23.
+//  Copyright © 2017 Adil Jiwani. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Adil Jiwani on 2017-10-23.
+//  Copyright © 2017 Adil Jiwani. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"

fc76bf355f23df8988ee934b24766b8bd174ac34
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-10-25 12:36:09 -0400

    login user complete

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -24,7 +24,11 @@ class CreateAccountVC: UIViewController {
         guard let pass = passTxt.text , passTxt.text != "" else {return}
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
-                print ("registered user")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        print ("logged in user", AuthService.instance.AuthToken)
+                    }
+                })
             }
         }
     }

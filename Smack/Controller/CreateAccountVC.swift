f2c934a641a6f89a8a5b1d2befa967ddccb90983
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-10-25 22:20:37 -0400

    avatar picker VC done

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -22,6 +22,13 @@ class CreateAccountVC: UIViewController {
         super.viewDidLoad()
 
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+    }
 
     @IBAction func createAccountPressed(_ sender: Any) {
         guard let name = usernameTxt.text , usernameTxt.text != "" else {return}
--- Smack/View/AvatarCell.swift
+++ Smack/View/AvatarCell.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+enum AvatarType {
+    case dark
+    case light
+    
+}
+
 class AvatarCell: UICollectionViewCell {
     
     @IBOutlet weak var avatarImg: UIImageView!
@@ -16,6 +22,16 @@ class AvatarCell: UICollectionViewCell {
         super.awakeFromNib()
         
         setupView()
+    }
+    
+    func configureCell (index: Int, type: AvatarType) {
+        if type == AvatarType.dark {
+            avatarImg.image = UIImage(named: "dark\(index)")
+            self.layer.backgroundColor = UIColor.lightGray.cgColor
+        } else {
+            avatarImg.image = UIImage(named: "light\(index)")
+            self.layer.backgroundColor = UIColor.gray.cgColor
+        }
     }
     
     func setupView () {

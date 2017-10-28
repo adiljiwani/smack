469b5bd32dca263070fb5fcf22cc0f64a8b24bd1
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-10-28 14:02:01 -0400

    login complete

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -15,6 +15,7 @@ let BASE_URL = "https://chataj.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
 
 // Colours
 let smackPurplePlaceholder = #colorLiteral(red: 0.2588235294, green: 0.3294117647, blue: 0.7254901961, alpha: 0.5)
@@ -32,6 +33,12 @@ let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
 
+// Headers
 let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
+
+let BEARER_HEADER = [
+    "Authorization": "Bearer \(AuthService.instance.AuthToken)",
     "Content-Type": "application/json; charset=utf-8"
 ]

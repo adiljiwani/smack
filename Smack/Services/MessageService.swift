89bdbc90e0b8074b342bcc196e5a5d643cafcc6b
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-11-01 12:20:02 -0400

    refined login flow

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -14,6 +14,7 @@ class MessageService {
     static let instance = MessageService()
     
     var channels = [Channel]()
+    var selectedChannel : Channel?
     
     func findAllChannel(completion: @escaping CompletionHandler) {
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
@@ -29,13 +30,17 @@ class MessageService {
                         self.channels.append(channel)
                     }
                     completion(true)
-                    //print(self.channels[0].channelTitle)
+                    NotificationCenter.default.post(name: NOTIF_CHANNELS_LOADED, object: nil)
                 }
                 
             } else {
                 completion(false)
-                debugPrint(response.result.error)
+                debugPrint(response.result.error as Any)
             }
         }
+    }
+    
+    func clearChannels() {
+        channels.removeAll()
     }
 }
